51851c9beda067be5d4e8b1124d74588e49ec91f
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-17 14:04:05 -0400

    formatting on routes

--- src/Assets/Admin/Routes.php
+++ src/Assets/Admin/Routes.php
@@ -1,66 +1,80 @@
 <?php
-
 namespace Assets\Admin;
 
 /**
  * Group class is used to keep track of a group of routes with similar aspects (the same controller, the same f3-app and etc)
  */
-class Routes extends \Dsc\Routes\Group{
-	
-	
-	function __construct(){
-		parent::__construct();
-	}
-	
-	/**
-	 * Initializes all routes for this group
-	 * NOTE: This method should be overriden by every group
-	 */
-	public function initialize(){
-		$this->setDefaults(
-				array(
-					'namespace' => '\Assets\Admin\Controllers',
-					'url_prefix' => '/admin'
-				)
-		);
-		
-		$this->addCrudGroup( 'Assets', 'Asset');
+class Routes extends \Dsc\Routes\Group
+{
 
-		$this->add( '/asset/rethumb/@id', 'GET', array(
-				'controller' => 'Asset',
-				'action' => 'rebuildThumb'
-		));
+    function __construct()
+    {
+        parent::__construct();
+    }
 
+    /**
+     * Initializes all routes for this group
+     * NOTE: This method should be overriden by every group
+     */
+    public function initialize()
+    {
+        $this->setDefaults( array(
+            'namespace' => '\Assets\Admin\Controllers',
+            'url_prefix' => '/admin' 
+        ) );
+        
+        $this->addCrudGroup( 'Assets', 'Asset' );
+        
+        $this->add( '/asset/rethumb/@id', 'GET', array(
+            'controller' => 'Asset',
+            'action' => 'rebuildThumb' 
+        ) );
+        
         // upload handlers
-		$this->add( '/asset/handleTraditional', 'POST', array(
-				'controller' => 'Asset',
-				'action' => 'handleTraditional'
-		));
-
-		$this->add( '/asset/handleS3', array('POST', 'DELETE'), array(
-				'controller' => 'Asset',
-				'action' => 'handleS3'
-		));
-
+        $this->add( '/asset/handleTraditional', 'POST', array(
+            'controller' => 'Asset',
+            'action' => 'handleTraditional' 
+        ) );
+        
+        $this->add( '/asset/handleS3', array(
+            'POST',
+            'DELETE' 
+        ), array(
+            'controller' => 'Asset',
+            'action' => 'handleS3' 
+        ) );
+        
         // element routes
-		$this->add( '/assets/element/@id', array('GET', 'POST'), array(
-				'controller' => 'Assets',
-				'action' => 'element'
-		));
-
-		$this->add( '/assets/element/@id/@page', array('GET', 'POST'), array(
-				'controller' => 'Assets',
-				'action' => 'element'
-		));
-
-		$this->add( '/assets/element/image/@id', array('GET', 'POST'), array(
-				'controller' => 'Assets',
-				'action' => 'elementImage'
-		));
-
-		$this->add( '/assets/element/image/@id/@page', array('GET', 'POST'), array(
-				'controller' => 'Assets',
-				'action' => 'elementImage'
-		));
-	}
+        $this->add( '/assets/element/@id', array(
+            'GET',
+            'POST' 
+        ), array(
+            'controller' => 'Assets',
+            'action' => 'element' 
+        ) );
+        
+        $this->add( '/assets/element/@id/@page', array(
+            'GET',
+            'POST' 
+        ), array(
+            'controller' => 'Assets',
+            'action' => 'element' 
+        ) );
+        
+        $this->add( '/assets/element/image/@id', array(
+            'GET',
+            'POST' 
+        ), array(
+            'controller' => 'Assets',
+            'action' => 'elementImage' 
+        ) );
+        
+        $this->add( '/assets/element/image/@id/@page', array(
+            'GET',
+            'POST' 
+        ), array(
+            'controller' => 'Assets',
+            'action' => 'elementImage' 
+        ) );
+    }
 }
