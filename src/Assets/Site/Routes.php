191c35531a42ea1e8d59c7caf878da3d0cb6649b
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-19 22:27:47 -0400

    formatting for routes class

--- src/Assets/Site/Routes.php
+++ src/Assets/Site/Routes.php
@@ -1,37 +1,25 @@
 <?php
-
 namespace Assets\Site;
 
-/**
- * Group class is used to keep track of a group of routes with similar aspects (the same controller, the same f3-app and etc)
- */
-class Routes extends \Dsc\Routes\Group{
-	
-	
-	function __construct(){
-		parent::__construct();
-	}
-	
-	/**
-	 * Initializes all routes for this group
-	 * NOTE: This method should be overriden by every group
-	 */
-	public function initialize(){
-		$this->setDefaults(
-				array(
-					'namespace' => '\Assets\Site\Controllers',
-					'url_prefix' => '/asset'
-				)
-		);
- 		// TODO Make this support dimensions, e.g. /asset/thumb/@slug/@width/@height
- 		$this->add( '/thumb/@slug', 'GET', array(
-								'controller' => 'Asset',
-								'action' => 'thumb'
-								));
+class Routes extends \Dsc\Routes\Group
+{
 
-		$this->add( '/@slug', 'GET', array(
-				'controller' => 'Asset',
-				'action' => 'read'
-		));
-	}
+    public function initialize()
+    {
+        $this->setDefaults(array(
+            'namespace' => '\Assets\Site\Controllers',
+            'url_prefix' => '/asset'
+        ));
+        
+        // TODO Make these both support dimensions, e.g. /asset/thumb/@slug/@width/@height
+        $this->add('/thumb/@slug', 'GET', array(
+            'controller' => 'Asset',
+            'action' => 'thumb'
+        ));
+        
+        $this->add('/@slug', 'GET', array(
+            'controller' => 'Asset',
+            'action' => 'read'
+        ));
+    }
 }
