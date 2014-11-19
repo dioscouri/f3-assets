83c34222d1cbfe42f86e8e7d07de9827fe7ea298
RallySport DMZ <chris.french@rallysportdirect.com>
2014-11-19 16:14:52 -0700

    opps reseting admin model

--- src/Assets/Admin/Models/Assets.php
+++ src/Assets/Admin/Models/Assets.php
@@ -1,27 +1,9 @@
 <?php 
-namespace Assets\Models;
+namespace Assets\Admin\Models;
 
 class Assets extends \Dsc\Mongo\Collections\Assets 
 {
-	
-	
-	public static function getStorage($type = 'gridfs') {
-		 
-		switch ($type) {
-			case "AmazonS3":
-				$storage = new \Assets\Models\Storage\AmazonS3;
-				break;
-			case "CloudFiles":
-				$storage = new \Assets\Models\Storage\CloudFiles;
-				break;
-			case "gridfs":
-			default:
-				$storage = new \Assets\Models\Storage\GridFs;
-		}
-		 
-		return $storage;
-	}
-	
+		
 	
 	
 	
